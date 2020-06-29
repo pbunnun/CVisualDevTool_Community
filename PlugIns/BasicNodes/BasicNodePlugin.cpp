a2ee996f035abae09d76e1cba34032ecfac80251
TSommart <thanapong.net@gmail.com>
2020-06-29 14:07:14 +0700

    - Added the Filter2D Node to Image Operation

--- PlugIns/BasicNodes/BasicNodePlugin.cpp
+++ PlugIns/BasicNodes/BasicNodePlugin.cpp
@@ -27,6 +27,7 @@
 #include "MorphologicalTransformationModel.hpp"
 #include "HoughCircleTransfromModel.hpp"
 #include "DistanceTransformModel.hpp"
+#include "Filter2DModel.hpp"
 
 QStringList BasicNodePlugin::registerDataModel( std::shared_ptr< DataModelRegistry > model_regs )
 {
@@ -58,6 +59,7 @@ QStringList BasicNodePlugin::registerDataModel( std::shared_ptr< DataModelRegist
     registerModel< MorphologicalTransformationModel >( model_regs, duplicate_model_names );
     registerModel< HoughCircleTransformModel >( model_regs, duplicate_model_names );
     registerModel< DistanceTransformModel >( model_regs, duplicate_model_names );
+    registerModel< Filter2DModel >( model_regs, duplicate_model_names );
     registerModel< CVCameraModel >( model_regs, duplicate_model_names );
     registerModel< TemplateModel >( model_regs, duplicate_model_names );
 
