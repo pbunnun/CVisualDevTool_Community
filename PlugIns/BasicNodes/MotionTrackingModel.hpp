58ae17965cb6347cebaaa3740b4f5d5af5f83dc8
DESKTOP-P3UICUF\Chana <chanatiwat@me.com>
2020-07-17 03:22:12 +0700

    - Added the MotionTracking Node to Image operation
    The node works best when moving objects are small or far from the camera.
    The trails are set to disappear after 2 seconds.
    
    Limitations:
    - If an object stops moving for longer than 0.5 second before moving again, it will be considered by the node as a new object.
    - The node cannot detect an moving object if it is too small or too slow.
    - The node can track an object despite it being obstruct by an object if the obstructing object is small enough and not moving.
    - The node has issues tracking a moving object when it pass through another moving object.

---
+++ PlugIns/BasicNodes/MotionTrackingModel.hpp
@@ -0,0 +1,65 @@
+#ifndef MOTIONTRACKINGMODEL_HPP
+#define MOTIONTRACKINGMODEL_HPP
+
+#pragma once
+
+#include <iostream>
+
+#include <QtCore/QObject>
+#include <QtWidgets/QLabel>
+
+#include <nodes/DataModelRegistry>
+#include "PBNodeDataModel.hpp"
+
+#include "CVImageData.hpp"
+
+using QtNodes::PortType;
+using QtNodes::PortIndex;
+using QtNodes::NodeData;
+using QtNodes::NodeDataType;
+using QtNodes::NodeValidationState;
+
+class MotionTrackingModel : public PBNodeDataModel
+{
+    Q_OBJECT
+
+public:
+    MotionTrackingModel();
+
+    virtual
+    ~MotionTrackingModel() override {}
+
+    unsigned int
+    nPorts(PortType portType) const override;
+
+    NodeDataType
+    dataType( PortType portType, PortIndex portIndex ) const override;
+
+    std::shared_ptr< NodeData >
+    outData( PortIndex port ) override;
+
+    void
+    setInData( std::shared_ptr< NodeData > nodeData, PortIndex ) override;
+
+    QWidget *
+    embeddedWidget() override { return nullptr; }
+
+    QPixmap
+    minPixmap() const override { return _minPixmap; }
+
+    static const QString _category;
+
+    static const QString _model_name;
+
+private:
+    std::shared_ptr< CVImageData > mpCVImageData;
+
+    QPixmap _minPixmap;
+
+    static void updateHistory(std::vector<cv::Rect> availableRect);
+
+    static cv::Scalar getColor(int placement);
+
+    static cv::Mat processData(const std::shared_ptr<CVImageData> &p);
+};
+#endif
